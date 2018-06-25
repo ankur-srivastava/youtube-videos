f95b9bd9a412e6afd6ca70a8f4710f7454636428
ankur-srivastava <ankurlkw@gmail.com>
2018-06-25 11:52:09 +0530

    V1

---
+++ connect/connect.js
@@ -0,0 +1,32 @@
+const request = require('request');
+
+var videoId = '';
+var api_key = 'AIzaSyBSDK94__J-mK8N6G-bSf4mpRsy9qn84YM';
+
+//For testing purpose
+var youtube_url = 'https://www.youtube.com/watch?v=Yl6ggYwxL3c&t=2s';
+var v = youtube_url.indexOf('v=');
+var endIndex = youtube_url.indexOf('&');
+
+videoId = youtube_url.substring((v+2),endIndex);
+var url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id='+videoId+'&key='+api_key;
+
+var videoRes = function(){
+  request({
+    url,
+    json: true
+  }, (err, res, body)=>{
+    if(err){
+      console.log(err);
+    }
+    var video = res.body.items[0];
+    var title = video.snippet.title;
+    var tags = video.snippet.tags;
+
+    console.log('----Title----',title);
+    console.log('----Tags----',tags);
+
+
+  });
+};
+videoRes();
